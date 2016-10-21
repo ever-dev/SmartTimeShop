e24b86a8ecbdd4647894ec1a9814cd96ab83d954
ever-dev <markcheng.shiny@gmail.com>
2016-10-21 08:32:56 +0400

    add sorting

--- app/routes/watches/watches.js
+++ app/routes/watches/watches.js
@@ -13,4 +13,20 @@ angular.module('smartTimeApp.watches', ['ngRoute'])
         $http.get('data/smartwatch.json').success(function(data) {
             $scope.watches = data;
         });
+
+        $scope.orderField = undefined;
+        $scope.orderReverse = false;
+
+        $scope.sort = function(field) {
+            $scope.orderReverse = $scope.orderField == field ? !$scope.orderReverse : false;
+            $scope.orderField = field;
+        }
+
+        $scope.isSortUp = function (field) {
+            return $scope.orderField == field && !$scope.orderReverse;
+        }
+
+        $scope.isSortDown = function (field) {
+            return $scope.orderField == field && $scope.orderReverse;
+        }
     }]);
